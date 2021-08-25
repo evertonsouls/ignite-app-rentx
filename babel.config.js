937d78c0168a2c8c279ae988f165d47c8f60c865
evertonsouls <evertonsouls@gmail.com>
2021-08-25 12:04:09 +0200

    Initialized project

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            tests: ['./tests/'],
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+          }
+        }
+      ]
+    ]
   };
 };
